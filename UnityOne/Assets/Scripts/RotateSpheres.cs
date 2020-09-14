6c84f0e87b96f9e1b4a16520fee11854730ed520
Siddharth <siddharth.pant@cognizant.com>
2020-09-14 12:26:20 -0600

    section 2 done

---
+++ UnityOne/Assets/Scripts/RotateSpheres.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSpheres : MonoBehaviour
+{
+	Vector3 movement;
+	public int xi, yi, zi;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     	movement = new Vector3(xi, yi, zi);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
